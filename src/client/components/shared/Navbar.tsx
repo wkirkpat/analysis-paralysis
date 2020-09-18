c57601b6f7433b5ee766c9211debe081ec8523b2
Will <wk1394@gmail.com>
2020-09-18 13:45:56 -0500

    navbar changes

--- src/client/components/shared/Navbar.tsx
+++ src/client/components/shared/Navbar.tsx
@@ -4,34 +4,58 @@ import { Link } from "react-router-dom";
 const Navbar: React.FC<INavbarProps> = () => {
   return (
     <>
-      <div
-        className="d-flex container-fluid justify-content-between bg-navbar p-2"
-        style={{ height: "4rem" }}
-      >
-        <div className="mt-2" style={{ width: "2rem", height: "2rem" }}>
-          <img
-            src="/assets/images/menu.png"
-            alt=""
-            className=" align-self-center img-fluid"
-          />
-        </div>
-        <div className="align-self-center h-100">
-          <Link to="/">
+      <nav className="navbar navbar-expand-md navbar-dark bg-navbar sticky-top">
+        <div
+          className="d-flex container-fluid justify-content-between justify-content-md-start bg-navbar p-2"
+          style={{ height: "4rem" }}
+        >
+          <div className="mt-2" style={{ width: "2rem", height: "2rem" }}>
             <img
-              src="/assets/images/logo4m.png"
+              src="/assets/images/menu.png"
               alt=""
-              className="align-self-center img-fluid h-100"
+              className="d-md-none align-self-center img-fluid"
             />
-          </Link>
+          </div>
+          <div className="h-100">
+            <Link to="/">
+              <img
+                src="/assets/images/logo4m.png"
+                alt=""
+                className="align-self-center img-fluid h-100"
+              />
+            </Link>
+          </div>
+          <div className="mt-2" style={{ width: "2rem", height: "2rem" }}>
+            <img
+              src="/assets/images/search.png"
+              alt=""
+              className="img-fluid align-self-center d-md-none"
+            />
+          </div>
+          <ul className="navbar-nav">
+            <li className="nav-item d-none d-md-inline">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item d-none d-md-inline">
+              <a className="nav-link" href="#">
+                Board Games
+              </a>
+            </li>
+            <li className="nav-item d-none d-md-inline">
+              <a className="nav-link" href="#">
+                Video Games
+              </a>
+            </li>
+            <li className="nav-item d-none d-md-inline">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="mt-2" style={{ width: "2rem", height: "2rem" }}>
-          <img
-            src="/assets/images/search.png"
-            alt=""
-            className="img-fluid align-self-center"
-          />
-        </div>
-      </div>
+      </nav>
     </>
   );
 };
